104d31879f01d332914cb8e15426e41a2ade7a10
JanaAdel1010 <janaadel2000@gmail.com>
2024-11-11 02:06:48 +0200

    thread for each element

--- Matrix mul thread.cc
+++ Matrix mul thread.cc
@@ -9,7 +9,7 @@ typedef struct{
 
 int row1,row2,col1,col2;
 int **matrix1, **matrix2, **result;
-void read(char* filename){
+void read(const char* filename){
     FILE *f=fopen(filename,"r");
     if(f==NULL){
         perror("Can't open file");
@@ -42,4 +42,59 @@ void read(char* filename){
         result[i]=(int *)malloc(col2*sizeof(int));
     }
 
+}
+void *element(void *elements)
+{
+    index *element_index=(index*)elements;
+    int sum=0;
+    for(int k=0;k<col1;k++)
+    {
+        sum+=matrix1[element_index->row][k]*matrix2[k][element_index->col];
+    }
+    result[element_index->row][element_index->col]=sum;
+    free(elements);
+    return NULL;
+}
+void matrixmul(){
+    pthread_t threads[row1*col2];
+    int counter=0;
+    clock_t start_time=clock();
+    for(int i=0;i<row1;i++)
+    {
+        for(int j=0;j<col2;j++)
+        {
+            index *indexx=(index*)malloc(sizeof(index));
+            indexx->row=i;
+            indexx->col=j;
+            pthread_create(&threads[counter++],NULL,element,indexx);
+        }
+    }
+    for(int i=0;i<counter;i++)
+    {
+        pthread_join(threads[i],NULL);
+    }
+    clock_t end_time=clock();
+    printf("Time taken is: %f seconds\n",(double)(end_time-start_time)/CLOCKS_PER_SEC);
+}
+void free_matrices() {
+    for (int i = 0; i < row1; i++) free(matrix1[i]);
+    for (int i = 0; i < row2; i++) free(matrix2[i]);
+    for (int i = 0; i < row1; i++) free(result[i]);
+    free(matrix1);
+    free(matrix2);
+    free(result);
+}
+int main()
+{
+    read("test.txt");
+    if(col1!=row2)
+    {
+        printf("incompatible matrices sizes");
+        free_matrices();
+        return 1;
+
+    }
+    matrixmul();
+    free_matrices();
+    return 0;
 }
